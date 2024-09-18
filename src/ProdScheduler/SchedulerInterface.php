cfe65ae4914f37cbb38b3afda023776871cf9277
yangwb1123 <1832227346@qq.com>
2024-09-18 14:03:12 +0800

    fix with php7

--- src/ProdScheduler/SchedulerInterface.php
+++ src/ProdScheduler/SchedulerInterface.php
@@ -16,12 +16,12 @@ namespace Sv\Algorithm\ProdScheduler;
 //     case Tenth = 10;
 // }
 
-enum ComputeDirection: int
-{
-    case Forward = 1;
-    case Reverse = 2;
-    case Both = 3;
-}
+// enum ComputeDirection: int
+// {
+//     case Forward = 1;
+//     case Reverse = 2;
+//     case Both = 3;
+// }
 
 interface SchedulerInterface
 {
